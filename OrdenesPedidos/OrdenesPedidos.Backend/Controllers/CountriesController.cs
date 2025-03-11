8f3b193de099472bb20d6faa203377d0b30a973c
AlexDev1979 <wilgar270@hotmail.com>
2025-03-11 16:12:10 -0600

    Agregar un controlador de api basico

---
+++ OrdenesPedidos/OrdenesPedidos.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrdenesPedidos.Backend.Data;
+using OrdenesPedidos.Shared.Entities;
+
+namespace OrdenesPedidos.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpGet ("{Id}")]
+        public async Task<IActionResult> GetAsync(int Id)
+        {
+            var country = await _context.Countries.FindAsync(Id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return  NoContent();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteAsync(int Id)
+        {
+            var country = await _context.Countries.FindAsync(Id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
