f25880b0d821a01186ccd44602db393a60134e24
AlexDev1979 <wilgar270@hotmail.com>
2025-03-11 13:27:52 -0600

    Creacion de base de datos y las migraciones pendientes

--- OrdenesPedidos/OrdenesPedidos.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OrdenesPedidos.Shared
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ OrdenesPedidos/OrdenesPedidos.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrdenesPedidos.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracter.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; } = null!;
+    }
+}
