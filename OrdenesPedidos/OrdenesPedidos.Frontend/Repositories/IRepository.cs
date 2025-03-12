0773239bb03cf64b21b3bf7624b2f2d478dbe2ed
AlexDev1979 <wilgar270@hotmail.com>
2025-03-12 11:29:00 -0600

    Patron inicial de repositorio

---
+++ OrdenesPedidos/OrdenesPedidos.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdenesPedidos.Frontend.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+        Task<HttpResponseWrapper<T>> PostAsync<T>(string url, T model);
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+    }
+}
